6fe0462371cd5acef496cf1a5c546518aaf634ba
Colyn <colyn@infrontlabs.com>
2018-05-30 09:06:39 -0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <div id="app"></div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
