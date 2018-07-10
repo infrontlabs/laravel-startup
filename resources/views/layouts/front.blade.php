fe73c322c65795a545387817a9dcb52d1dce5342
Colyn <colyn.brown@gmail.com>
2018-07-10 12:22:56 -0700

    added flash messaging

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.base')
 
 @section('base.content')
+    <div class="container">
+        @if(session('error'))
+        <div class="alert alert-danger" role="alert">
+            {!! session('error') !!}
+        </div>
+        @endif
+        @if(session('success'))
+        <div class="alert alert-success" role="alert">
+            {!! session('success') !!}
+        </div>
+        @endif
+    </div>
     @yield('content')
 @endsection
